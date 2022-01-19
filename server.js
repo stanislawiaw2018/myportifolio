8f7d5204263206f53a53bdc7e62818c53b7dd625
Antonio Stanislaw <stanislaw.aiw2018@gmail.com>
2022-01-19 01:18:55 -0300

    instalation e configuration of nunjucks dependency

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
+
+server.set("view engine", "html");
+
+nunjucks.configure("views", {
+    express:   server
+})
 
 // ROTAS
 server.get( "/home" ,  function(req, res) {
         return res.send("ok,  are you? how are you name?");
-})
+});
 
 server.listen(5001, function () {
     console.log('listening on port');
