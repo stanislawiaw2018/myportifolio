2a109a1af1801723f3d1b863de39bff88f71032c
Antonio Stanislaw <stanislaw.aiw2018@gmail.com>
2022-01-19 01:58:20 -0300

    bringing the website front to the server

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const nunjucks = require('nunjucks');
 
 const server = express();
 
+// BUSCA ONDE ESTÃO ARQUIVOS ESTATICOS
+server.use(express.static('public'));
+
+//caracteriza a extensão do arquivo
 server.set("view engine", "html");
 
+//pasta onde fica os arquivos estaticos
 nunjucks.configure("views", {
     express:   server
 })
 
-// ROTAS
-server.get( "/home" ,  function(req, res) {
-        return res.send("ok,  are you? how are you name?");
+// ######ROTAS########
+server.get( "/" ,  function(req, res) {
+        return res.render("index");
+});
+server.get( "/projects" ,  function(req, res) {
+        return res.render("projects");
 });
 
 server.listen(5001, function () {
